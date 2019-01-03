77773aa5d51e12ede2b305898300ba4f2148e1c2
MuraliJegathisan <43337932+MuraliJegathisan@users.noreply.github.com>
2019-01-03 14:26:33 +0530

    Create codeKata4.c

---
+++ codeKata4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    char a;
+    printf("INPUT:\n");
+    scanf("%c",&a);
+    if(a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h'||a=='i'||a=='j'||a=='k'||a=='l'||a=='m'||a=='n'||a=='o'||a=='p'||a=='q'||a=='r'||a=='s'||a=='t'||a=='u'||a=='v'||a=='w'||a=='x'||a=='y'||a=='z')
+    {
+        printf("Output:\n");
+        printf("Alphabet");
+    }
+    else
+    {
+        printf("Output:\n");
+        printf("No");
+    }
+
+}
