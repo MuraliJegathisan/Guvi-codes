379cc6195cea5ee4e01fdeb1b22f8fd6ce7154f6
MuraliJegathisan <43337932+MuraliJegathisan@users.noreply.github.com>
2019-01-22 12:04:22 +0530

    Create codekata7.c

---
+++ codekata7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n;
+    clrscr();
+    printf("Input\n");
+    scanf("%d",&n);
+    printf("Output\n");
+    for(i=1;i<=n;i++)
+    {
+        printf("Hello\n");
+    }
+    getch();
+    
+}
